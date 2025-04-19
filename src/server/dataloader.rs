c79bcb4130051d16b3409187300bdcdadc25023f
Arpad Borsos <swatinem@swatinem.de>
2025-04-19 12:36:13 +0200

    simple implementation

--- src/server/dataloader.rs
+++ src/server/dataloader.rs
@@ -15,14 +15,16 @@ pub trait BatchLoader {
     async fn load_batch(&mut self, keys: &[Self::K]) -> HashMap<Self::K, Self::V>;
 }
 
-struct LoaderInner<B> {
+struct LoaderInner<B: BatchLoader> {
     load_batch: B,
+    requested_keys: Vec<B::K>,
+    resolved_values: HashMap<B::K, B::V>,
 }
 
-pub struct DataLoader<B> {
+pub struct DataLoader<B: BatchLoader> {
     inner: Arc<Mutex<LoaderInner<B>>>,
 }
-impl<B> Clone for DataLoader<B> {
+impl<B: BatchLoader> Clone for DataLoader<B> {
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
@@ -30,79 +32,39 @@ impl<B> Clone for DataLoader<B> {
     }
 }
 
-impl<B> DataLoader<B>
+impl<B: BatchLoader> DataLoader<B>
 where
-    B: BatchLoader,
     B::K: Debug,
 {
     pub fn new(load_batch: B) -> Self {
-        let inner = LoaderInner { load_batch };
+        let inner = LoaderInner {
+            load_batch,
+            requested_keys: Default::default(),
+            resolved_values: Default::default(),
+        };
         Self {
             inner: Arc::new(Mutex::new(inner)),
         }
     }
 
-    pub fn load(&self, key: B::K) -> impl Future<Output = B::V> {
-        async move {
-            println!("starting to resolve related objects for `{key:?}`");
-            yield_now().await;
-
+    pub async fn load(&self, key: B::K) -> B::V {
+        {
             let mut inner = self.inner.lock().await;
-            inner
-                .load_batch
-                .load_batch(&[key.clone()])
-                .await
-                .get(&key)
-                .unwrap()
-                .clone()
+            println!("starting to resolve related objects for `{key:?}`");
+            inner.requested_keys.push(key.clone());
         }
-        // LoadFuture::new(self, key)
-    }
-}
+        yield_now().await;
 
-enum LoadFuture<'l, B>
-where
-    B: BatchLoader,
-{
-    Created {
-        loader: &'l DataLoader<B>,
-        key: B::K,
-    },
-    Pending {
-        loader: &'l DataLoader<B>,
-        key: B::K,
-    },
-    Ready(B::V),
-}
-
-impl<'l, B> LoadFuture<'l, B>
-where
-    B: BatchLoader,
-{
-    fn new(loader: &'l DataLoader<B>, key: B::K) -> Self {
-        Self::Created { loader, key }
-    }
-}
-
-impl<'l, B> Future for LoadFuture<'l, B>
-where
-    B: BatchLoader,
-    B::K: Debug,
-{
-    type Output = B::V;
-
-    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-        let n = std::any::type_name::<B::V>();
-        todo!()
-        // match self {
-        //     LoadFuture::Created { loader, key } => {
-        //         todo!();
-        //         println!("starting to resolve `{N}` by `{key:?}`");
-        //         *self = LoadFuture::Pending { loader, key };
-        //         Poll::Pending
-        //     }
-        //     LoadFuture::Pending { loader, key } => todo!(),
-        //     LoadFuture::Ready(v) => Poll::Ready(v),
-        // }
+        let mut inner = self.inner.lock().await;
+        let requested_keys = std::mem::take(&mut inner.requested_keys);
+        if !requested_keys.is_empty() {
+            let resolved_values = inner.load_batch.load_batch(&requested_keys).await;
+            inner.resolved_values.extend(resolved_values);
+        }
+        inner
+            .resolved_values
+            .get(&key)
+            .expect("value should have been loaded")
+            .clone()
     }
 }
